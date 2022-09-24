7956e7e1aa8471f336decd41d39554057c84526c
greyonion404 <twitterpicker111@gmail.com>
2022-09-25 00:25:55 +0600

    updated to get authorID

--- pages/api/generate-winner-for-bot.js
+++ pages/api/generate-winner-for-bot.js
@@ -1,10 +1,10 @@
-import { Client } from "twitter-api-sdk";
+import { auth, Client } from "twitter-api-sdk";
 import { addTokenToDatabase, getMostViableToken } from "../../database/token";
 import { addWinnerToDatabase } from "../../database/winner";
 import { getAllRetweetersOfTweetID, getRandomRetweeter } from "./get-random-retweeter";
 
 
-async function getAuthorfromTweetID(token, tweetID) {
+async function getAuthorIdfromTweetID(token, tweetID) {
     try {
         const client = new Client(token);
         const lookupTweetById = await client.tweets.findTweetById(
@@ -14,14 +14,14 @@ async function getAuthorfromTweetID(token, tweetID) {
                 "user.fields": ["name"],
             }
         );
-        return lookupTweetById?.includes?.users[0];
+        return lookupTweetById?.includes?.users[0]?.id;
     }
     catch (error) {
         return null;
     }
 }
 
-const retweetLimit = 300;
+const retweetLimit = 100;
 export default async function handler(request, response) {
 
     // if not a post request! No business being here, fuck off punk.
@@ -43,7 +43,7 @@ export default async function handler(request, response) {
 
     let twitterToken = await getMostViableToken();
 
-    let author = await getAuthorfromTweetID(twitterToken, tweetID);
+    let authorID = await getAuthorIdfromTweetID(twitterToken, tweetID);
 
     //  fetch all retweeters, request count, fetched document count using the method below
     let { retweeters, numOfDocuments, numOfRequests } = await getAllRetweetersOfTweetID(twitterToken.token, tweetID, retweetLimit);
@@ -56,7 +56,7 @@ export default async function handler(request, response) {
 
     if (randomRetweeter && databaseResponse.data) {
         let winner = randomRetweeter;
-        let message = "A winner was selected for the given tweet. Winner is : @" + winner.handle + JSON.stringify(author) +
+        let message = authorID + "A winner was selected for the given tweet. Winner is : @" + winner.handle +
             "\nTo visit the winner, go to: https://twitter.com/" + winner.handle + ".\n";
         response.json({ message: message });
         return;
